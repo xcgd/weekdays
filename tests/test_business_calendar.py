c079fa4a482996209dc0ac389204c78c5bc0d69d
Florent Aide <florent.aide@gmail.com>
2013-11-06 16:05:43 +0100

    Added an new get_weeks_daycount helper method + tests

---
+++ tests/test_business_calendar.py
@@ -0,0 +1,96 @@
+import calendar
+import datetime
+from dateutil import rrule
+from weekdays import BusinessCalendar
+
+
+def test_calendar_list_of_days():
+    year = 2013
+    month = 11
+    weekstartday = calendar.MONDAY
+    weekoff = [rrule.SA, rrule.SU]
+    holidays = [
+        datetime.datetime(2013, 11, 1),
+        datetime.datetime(2013, 11, 11),
+    ]
+    c = BusinessCalendar(weekstartday, weekoff, holidays)
+    res = c.get_month_business_days(year, month)
+    assert res == [
+        datetime.datetime(2013, 11, 4, 0, 0),
+        datetime.datetime(2013, 11, 5, 0, 0),
+        datetime.datetime(2013, 11, 6, 0, 0),
+        datetime.datetime(2013, 11, 7, 0, 0),
+        datetime.datetime(2013, 11, 8, 0, 0),
+        datetime.datetime(2013, 11, 12, 0, 0),
+        datetime.datetime(2013, 11, 13, 0, 0),
+        datetime.datetime(2013, 11, 14, 0, 0),
+        datetime.datetime(2013, 11, 15, 0, 0),
+        datetime.datetime(2013, 11, 18, 0, 0),
+        datetime.datetime(2013, 11, 19, 0, 0),
+        datetime.datetime(2013, 11, 20, 0, 0),
+        datetime.datetime(2013, 11, 21, 0, 0),
+        datetime.datetime(2013, 11, 22, 0, 0),
+        datetime.datetime(2013, 11, 25, 0, 0),
+        datetime.datetime(2013, 11, 26, 0, 0),
+        datetime.datetime(2013, 11, 27, 0, 0),
+        datetime.datetime(2013, 11, 28, 0, 0),
+        datetime.datetime(2013, 11, 29, 0, 0),
+    ]
+
+
+def test_calendar_list_of_weeks():
+    year = 2013
+    month = 11
+    weekstartday = calendar.MONDAY
+    weekoff = [rrule.SA, rrule.SU]
+    holidays = [
+        datetime.datetime(2013, 11, 1),
+        datetime.datetime(2013, 11, 11),
+    ]
+    c = BusinessCalendar(weekstartday, weekoff, holidays)
+    res = c.get_weeks_business_days(year, month)
+
+    assert res == [
+        [],
+        [
+            datetime.datetime(2013, 11, 4, 0, 0),
+            datetime.datetime(2013, 11, 5, 0, 0),
+            datetime.datetime(2013, 11, 6, 0, 0),
+            datetime.datetime(2013, 11, 7, 0, 0),
+            datetime.datetime(2013, 11, 8, 0, 0),
+        ],
+        [
+            datetime.datetime(2013, 11, 12, 0, 0),
+            datetime.datetime(2013, 11, 13, 0, 0),
+            datetime.datetime(2013, 11, 14, 0, 0),
+            datetime.datetime(2013, 11, 15, 0, 0),
+        ],
+        [
+            datetime.datetime(2013, 11, 18, 0, 0),
+            datetime.datetime(2013, 11, 19, 0, 0),
+            datetime.datetime(2013, 11, 20, 0, 0),
+            datetime.datetime(2013, 11, 21, 0, 0),
+            datetime.datetime(2013, 11, 22, 0, 0),
+        ],
+        [
+            datetime.datetime(2013, 11, 25, 0, 0),
+            datetime.datetime(2013, 11, 26, 0, 0),
+            datetime.datetime(2013, 11, 27, 0, 0),
+            datetime.datetime(2013, 11, 28, 0, 0),
+            datetime.datetime(2013, 11, 29, 0, 0),
+        ],
+    ]
+
+
+def test_calendar_list_of_week_count():
+    year = 2013
+    month = 11
+    weekstartday = calendar.MONDAY
+    weekoff = [rrule.SA, rrule.SU]
+    holidays = [
+        datetime.datetime(2013, 11, 1),
+        datetime.datetime(2013, 11, 11),
+    ]
+    c = BusinessCalendar(weekstartday, weekoff, holidays)
+    res = c.get_weeks_daycount(year, month)
+    assert res == [0, 5, 4, 5, 5]
--- weekdays/helpers.py
+++ weekdays/helpers.py
@@ -23,3 +23,33 @@ class BusinessCalendar(calendar.Calendar):
         end = datetime.datetime(year, month, lastdaynum, 0, 0)
 
         return get_business_days(start, end, self.weekoff, self.holidays)
+
+    def get_weeks_business_days(self, year, month):
+        """given an year and month returns the business days for this month
+        ordered in lists of weeks.
+        """
+        business_weeks = []
+        # this is the naive list of days sorted in weeks
+        allweeksdays = self.monthdatescalendar(year, month)
+
+        monthbusinessdays = self.get_month_business_days(year, month)
+
+        for week in allweeksdays:
+            weekbusinessdays = []
+            for day in week:
+                daydt = datetime.datetime(day.year, day.month, day.day, 0, 0)
+                if daydt in monthbusinessdays:
+                    weekbusinessdays.append(daydt)
+
+            business_weeks.append(weekbusinessdays)
+
+        return business_weeks
+
+    def get_weeks_daycount(self, year, month):
+        """given a year and month returns a list of business days count
+        for all the weeks of the month.
+        ie: [0, 5, 4, 5, 5]
+        """
+        return [
+            len(week) for week in self.get_weeks_business_days(year, month)
+        ]
